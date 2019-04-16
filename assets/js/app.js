12092d0f9f438b4c8072003db4db3f5e7265a35a
Roberto Castanon <robertocastanon925@gmail.com>
2019-04-15 17:51:24 -0700

    gave each button a dataset and a corresponding name

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,45 @@
 // JS
-// document.querySelector("#container").addEventListener("click", function(event) {
 
-//     if (event.target.tagName == "BUTTON") {
-
-//         var gif = event.target.dataset.gif;
-
-//         var queryURL = `https://api.giphy.com/v1/gifs/search?q="${gif}&api_key=C1f3HAJEWDoZZ7SkLq487LAiHvdOMfgP&limit=10`;
-
-//         fetch(queryURL, {
-//             method: "GET"
-//         })
-
-//         .then(function(response) {return response.json()})
-//         .then(function(response) {
-//             console.log(queryURL);
-//             console.log(response);
-
-//             var results = response.data;
-
-//             for (let item of results) {
-
-//                 var gifDiv = document.createElement("div");
-
-//                 var p = document.createElement("p")
-//                 p.innerText = `Rating: ${item.rating}`;
-
-//                 var gifImage = document.createElement("img");
-
-//                 gifImage.setAttribute("src", item.images.fixed_height.url);
-
-//                 gifDiv.appendChild(p);
-//                 gifDiv.appendChild(gifImage);
-
-//                 let gifContainer = document.querySelector("#gifs-here");
-//                 gifContainer.prepend(gifDiv);
-//             }
-//         })
-//     }
-// })
-
-var topics =['cat','dog','pig','turtle','fish', 'pangolin'];
+var topics =['cat','dog','pig','turtle','fish','pangolin'];
 
 function printBtn() {
     for (var i = 0; i < topics.length; i++) {
-       var btn = document.createElement("button");
+       var btn = document.createElement('button');
+       
        var t = document.createTextNode(topics[i]);
        btn.appendChild(t);
        document.body.appendChild(btn);
-       btn.setAttribute("id", "gifBtn");
+    //    btn.setAttribute("id", "gif");
+       btn.setAttribute("data-gif", topics[i]);
 
     }
 }
 
 printBtn();
+
+
+
+// var topics =['cat','dog','pig','turtle','fish','pangolin'];
+
+// function printBtn() {
+//     for (var i = 0; i < topics.length; i++) {
+//        var btn = document.createElement('button');
+//        var t = document.createTextNode(topics[i]);
+//        btn.appendChild(t);
+//        document.body.appendChild(btn);
+//        btn.setAttribute("id", "gif");
+
+//     }
+// }
+
+// printBtn();
+
+
+
+
+
+
+
 
 
 
